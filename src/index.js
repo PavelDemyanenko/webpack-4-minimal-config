655346e89b9699a5a4a559711061c8e75f6c4766
PavelDemyanenko <mail@paveldemyanenko.com>
2018-06-03 00:36:34 +0300

    Stackoverflow qa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import _ from 'lodash';
 import './scss/main.scss';
 
-console.log('My Minimal React Webpack Babel Setup');
+$(() => {
+  $(window).on('scroll', function() {
+    let scrollTop = $(this).scrollTop();
+    $('.b-main_section').each(function() {
+      let el = $(this);
+        let topDistance = el.offset().top;
+        if (topDistance < scrollTop) {
+          if (el.innerHeight() < el[0].scrollHeight) {
+            console.log('el.innerHeight()', el.innerHeight())
+            console.log('el[0].scrollHeight', el[0].scrollHeight)
+            $(window).on('scroll.prevent');
+          }
+        }
+    });
+  });
+
+  $('.b-main_section').on('scroll', function() {
+    let el = $(this);
+    console.log('scrolling')
+    if (el.scrollTop() + el.innerHeight() >= el[0].scrollHeight) {
+      $(window).off('scroll.prevent');
+      console.log('end reached');
+    }
+  });
+});
