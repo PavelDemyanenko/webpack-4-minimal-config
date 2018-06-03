f14d2c22be21e0fb33effab1342dfbe4b614cbfa
PavelDemyanenko <mail@paveldemyanenko.com>
2018-06-03 10:11:20 +0300

    Concept for scrolling sections

--- src/index.js
+++ src/index.js
@@ -2,27 +2,40 @@ import _ from 'lodash';
 import './scss/main.scss';
 
 $(() => {
+  $('body').data('skip-element', false);
+  let isInViewport = function(el) {
+    let elementTop = el.offset().top;
+    let elementBottom = elementTop + el.outerHeight();
+    let viewportTop = $(window).scrollTop();
+    let viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
   $(window).on('scroll', function() {
     let scrollTop = $(this).scrollTop();
     $('.b-main_section').each(function() {
       let el = $(this);
-        let topDistance = el.offset().top;
-        if (topDistance < scrollTop) {
-          if (el.innerHeight() < el[0].scrollHeight) {
-            console.log('el.innerHeight()', el.innerHeight())
-            console.log('el[0].scrollHeight', el[0].scrollHeight)
-            $(window).on('scroll.prevent');
+      let topDistance = el.offset().top;
+      if (topDistance < scrollTop) {
+        if (el.innerHeight() < el[0].scrollHeight) {
+          if (isInViewport(el)) {
+            if (!$('body').data('skip-element')) {
+              $('body').addClass('stop-scrolling');
+              $('body').data('skip-element', true);
+            }
           }
         }
+      }
     });
   });
 
   $('.b-main_section').on('scroll', function() {
     let el = $(this);
-    console.log('scrolling')
+    $('body').data('skip-element', true);
     if (el.scrollTop() + el.innerHeight() >= el[0].scrollHeight) {
-      $(window).off('scroll.prevent');
-      console.log('end reached');
+      $('body').removeClass('stop-scrolling')
+    } else if (el.scrollTop() == 0) {
+      console.log('top reached');
     }
   });
 });
